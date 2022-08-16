2537559c165b2495c205020127bb6778967a4628
Karthikeyan <karthikeyanthanigachalam@gmail.com>
2022-08-16 20:56:26 +0530

    Added: Gambling Game UC1, UC2 & UC3

---
+++ GamblingUC1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Gambling;
+
+public class GamblingUC1 {
+    public static final int BET = 1;
+    public static final int AMOUNT = 100;
+    public static void main(String[] args) {
+
+        int won = 0;
+        System.out.println("Initial Stake is " +AMOUNT+ "$ and bet is " +BET+"$");
+    }
+}
---
+++ GamblingUC2.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.Gambling;
+
+public class GamblingUC2 {
+    public static final int BET = 1;
+    public static final int AMOUNT = 100;
+    public static void main(String[] args) {
+
+        int won = 0;
+        int bet = (int) (Math.floor(Math.random()*10)%2);
+        if (bet == 1){
+            System.out.println("Won $1");
+            System.out.println("Initial Stake is $" +AMOUNT+ ", won $1 "+"and total is $100");
+        }else {
+            System.out.println("Loss $1");
+            System.out.println("Initial Stake is $" + AMOUNT + ", lost $1 "+"and total is $99");
+        }
+    }
+}
